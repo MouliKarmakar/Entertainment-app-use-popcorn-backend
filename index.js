ea4d5818d043d06baa01fe30781879adcd1cb80d
MouliKarmakar <moulikarmakar7596@gmail.com>
2024-06-23 12:55:17 +0530

    Added atlas string as DBURI to connenct with database

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const app = require("./app"); // Correctly require app
 const port = process.env.PORT || 10000;
 
 // Connect to DATABASE
-mongoose.connect(process.env.DBURI || "mongodb://localhost:27017/popcornuser", {
+// || "mongodb://localhost:27017/popcornuser"
+mongoose.connect(process.env.DBURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 const db = mongoose.connection;
 db.on("error", (err) => console.log(err));
-db.once("open", () => console.log("Connected to database"));
+db.once("open", () => console.log("Connected to database", process.env.DBURI));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
